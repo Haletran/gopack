6f9682aaf7a2e9daf180f3030de2d08c436e0048
Haletran <bpasquier123@gmail.com>
2026-03-23 15:53:35 +0100

    feat: search feature working with according database

---
+++ installer.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type Package struct {
+	Name        string
+	Description string
+	Author      string
+	Latest      string
+	Versions    []string
+}
+
+type Index struct {
+	Packages []Package
+}
+
+func checkdatabase() {
+	_, err := os.Open("index.json")
+	if err != nil {
+		fmt.Println("Index not found, Downloading the list of all packages...")
+		// maybe consider having multiple sources of database instead of only one ?? or add possibility to change the source
+		resp, err := http.Get("https://raw.githubusercontent.com/Haletran/gopack/refs/heads/main/index.json?token=GHSAT0AAAAAADTS6R6DASPL3CESDOSRM4KI2OBLEWQ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		f, err := os.Create("index.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		if _, err := io.Copy(f, resp.Body); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func SearchCommand(package_name string) {
+	fmt.Println("Searching for", package_name)
+	checkdatabase()
+	data, err := os.ReadFile("index.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var index Index
+	json.Unmarshal(data, &index) // TODO : need to add error check here in case of a wrong json file
+
+	for _, pkg := range index.Packages {
+		if strings.Compare(pkg.Name, package_name) == 0 {
+			fmt.Println("---")
+			fmt.Printf("Name: %s\n", pkg.Name)
+			fmt.Printf("Description: %s\n", pkg.Description)
+			fmt.Printf("Author: %s\n", pkg.Author)
+			fmt.Printf("Latest: %s\n", pkg.Latest)
+			fmt.Printf("Versions: %v\n", pkg.Versions)
+			fmt.Println("---")
+		}
+	}
+}
--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func command_parser(command_args []string) {
 	case "uninstall":
 		fmt.Println("Uninstalling", command_args[2])
 	case "search":
-		fmt.Println("Searching for", command_args[2])
+		SearchCommand(command_args[2])
 	default:
 		fmt.Println("Unknown command", command_args[1])
 		os.Exit(1)
